8ba937b09c701dd6b3b1282f5677182542de2d8d
袁德桥 <2960428571@qq.com>
2018-09-23 20:40:29 +0800

    侃侃-清理未使用的函数和日志

--- 看看侃侃电影/client/pages/add-comment/add-comment.js
+++ 看看侃侃电影/client/pages/add-comment/add-comment.js
@@ -17,13 +17,6 @@ Page({
     setRecordOrAudio:0,//抛出这个控制变量到页面
 
     addcomments: {},
-    //   id: 1,
-    //   title: "复仇者联盟3：无限战争",
-    //   category: "动作 / 科幻 / 奇幻 / 冒险",
-    //   image: "https://movies-1257217617.cos.ap-shanghai.myqcloud.com/p2517753454.jpg",
-    //   description: '《复仇者联盟3：无限战争》是漫威电影宇宙10周年的历史性集结，将为影迷们带来史诗版的终极对决。面对灭霸突然发起的闪电袭击，复仇者联盟及其所有超级英雄盟友必须全力以赴，才能阻止他对全宇宙造成毁灭性的打击。',
-    //   create_time: "2018-08-26 21:39:25"
-    // }
   },
 
   /**
@@ -130,59 +123,4 @@ Page({
   //     console.log(res.errCode)
   //   })
   // },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    // // 同步授权状态
-    // app.checkSession({
-    //   success: ({ userInfo }) => {
-    //     this.setData({
-    //       userInfo
-    //     })
-    //   }
-    // })
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
--- 看看侃侃电影/client/pages/index/index.js
+++ 看看侃侃电影/client/pages/index/index.js
@@ -12,14 +12,6 @@ Page({
     locationAuthType: app.data.locationAuthType,
     getComment:{},
     gethotmovie:{}
-    // gethotmovie: {
-    //   id: 1,
-    //   title: "复仇者联盟3：无限战争",
-    //   category: "动作 / 科幻 / 奇幻 / 冒险",
-    //   image: "https://movies-1257217617.cos.ap-shanghai.myqcloud.com/p2517753454.jpg",
-    //   description: '《复仇者联盟3：无限战争》是漫威电影宇宙10周年的历史性集结，将为影迷们带来史诗版的终极对决。面对灭霸突然发起的闪电袭击，复仇者联盟及其所有超级英雄盟友必须全力以赴，才能阻止他对全宇宙造成毁灭性的打击。',
-    //   create_time: "2018-08-26 21:39:25"
-    // }
   },
   onPullDownRefresh(){
     this.getMovieLists(()=>{
@@ -42,10 +34,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let getCurrUserInfo = this.data.userInfo
-    if (!!!getCurrUserInfo) {
-      this.getMovieLists()
-    }
+    this.getMovieLists()
   },
   topDetail(){
     // url = "/pages/top-detail/top-detail"
@@ -146,6 +135,9 @@ Page({
           userInfo,
           locationAuthType: app.data.locationAuthType
         })
+        // 登录成功进入主页
+        this.getMovieLists()
+
       },
       error: () => {
         this.setData({
@@ -153,13 +145,6 @@ Page({
         })
       }
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
   },
 
   /**
@@ -179,38 +164,4 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  // /**
-  //  * 页面相关事件处理函数--监听用户下拉动作
-  //  */
-  // onPullDownRefresh: function () {
-
-  // },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
